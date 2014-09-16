b0c41b2304fe2a21bd7cb422adf240dd71c9a2c3
Kristi Henno <hennokri@gmail.com>
2014-09-16 17:40:09 +0300

    Add sample comments'
    Add sample comments'

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>harjutus-2 - PHP põhitõed</title>
-  <?php echo "See siin on meelepärane string"; 
+</head>
+
+<body>
+    <h1>See dokument on minu esimene katsetus PHP-ga</h1>
+	<?php echo "See siin on meelepärane string"; 
 		echo "<br>";
 		echo "Sinu teksti esimene pool" . " ja sinu teksti teine pool.";?>
 		 <br>
@@ -14,9 +18,16 @@
 	 <?php echo 2 / 4; ?>
 	 <br>
 	 <?php echo 4 - 2; ?>
-</head>
+	 
+<?php // Selline on üks variant üherealisest kommentaarist. ?>
+<?php # Selline on teine variant üherealisest kommentaarist. ?>
+<?php
+    /*
+        See
+		on mitmerealine
+		kommentaar.
+    */
+?>
 
-<body>
-    <h1>See dokument on minu esimene katsetus PHP-ga</h1>
 </body>
 </html>
