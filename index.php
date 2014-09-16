09bdeab20a67f74fee8323680bf683785042ebc2
Kristi Henno <hennokri@gmail.com>
2014-09-16 17:45:52 +0300

    Add sample variables'
    Add sample variables'

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 		echo "<br>";
 		echo "Sinu teksti esimene pool" . " ja sinu teksti teine pool.";?>
 		 <br>
+<h1>stringid/arvutamine</h1>
 	<?php echo 2 + 3; ?>	
 	 <br>
 	 <?php echo 2 * 4; ?>
@@ -28,6 +29,22 @@
 		kommentaar.
     */
 ?>
+<br>
+<h1>Muutujad</h1>
+<?php 
+	 $number = "Tekst";
+	 $Number = 8 - 2;
+	 $text = "Kahe arvu summa on";
 
+	 echo $number;
+	 echo "<br>";
+	 echo $Number;
+	 echo "<p>Siia tuleb list</p>
+	 	<ul>
+	 		<li>$text</li>
+	 		<li>($number + $Number)</li>
+	 	</ul>";
+	 echo $number + $Number;
+	  ?>
 </body>
 </html>
